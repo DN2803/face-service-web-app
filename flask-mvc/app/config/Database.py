afea8b29e6a98d6b8397b7ae95eaef92aba4c803
dn28203 <ntkngann2k3@gmail.com>
2024-10-26 11:14:34 +0700

    update create database

--- flask-mvc/app/config/Database.py
+++ flask-mvc/app/config/Database.py
@@ -1,9 +1,18 @@
-from pymongo import MongoClient
+# from pymongo import MongoClient
 
-# Kết nối đến MongoDB (thay đổi URL nếu cần)
-client = MongoClient('mongodb://localhost:27017/')
+# # Kết nối đến MongoDB (thay đổi URL nếu cần)
+# client = MongoClient('mongodb://localhost:27017/')
 
-# Lựa chọn database
-db = client['demo-app-react-face-auth']
+# # Lựa chọn database
+# db = client['demo-app-react-face-auth']
 
+import mysql.connector
+
+connection = mysql.connector(
+    host = "localhost",
+    user = "root",
+    password = ""
+)
+
+cursor = connection.cursor()
 
