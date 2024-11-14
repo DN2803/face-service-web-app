c9e39a6ae7f0906d7c0e2efabe4c8d4ebd0675e2
huulockt <huulocnkt@gmail.com>
2024-11-15 02:30:47 +0700

    add/get project .

--- flask-mvc/app/packages/api/controllers/api.py
+++ flask-mvc/app/packages/api/controllers/api.py
@@ -1,17 +1,17 @@
-from flask import Blueprint, request, jsonify
-from app.packages.api.services.KeyService import KeyService
+# from flask import Blueprint, request, jsonify
+# from app.packages.api.services.KeyService import KeyService
 
-api_bp = Blueprint('api', __name__, url_prefix='/api/project')
+# api_bp = Blueprint('api', __name__, url_prefix='/api/project')
 
-@api_bp.route('/add/person', methods=['POST'])
-def add_person():
-    try:
-        data = request.json
-        key, exp = UserService().create_project(user_id, data['project_name'])
+# @api_bp.route('/add/person', methods=['POST'])
+# def add_person():
+#     try:
+#         data = request.json
+#         key, exp = UserService().create_project(user_id, data['project_name'])
 
-        return jsonify(key=key, exp=exp), 201
+#         return jsonify(key=key, exp=exp), 201
 
-    except Exception as e:
-        print(e)
-        return jsonify(error=str(e)), 400
+#     except Exception as e:
+#         print(e)
+#         return jsonify(error=str(e)), 400
     
