f25dfbf3634bf645b8696db4962a1b652c30dcd5
dn28203 <ntkngann2k3@gmail.com>
2024-12-13 21:33:21 +0700

    fix logic add dev, slider bar bát đồng bộ

--- berry-free-react-admin-template-3.4.1/src/hooks/index.js
+++ berry-free-react-admin-template-3.4.1/src/hooks/index.js
@@ -1,5 +1,5 @@
 export { default as useEmailVerified } from './state/useEmailVerified';
-
+export { default as useAdminStatus } from './state/useAdminStatus'
 
 export { useEmail } from './context/EmailContext';
 export { useUserInfo} from './context/UserInfoContext'; 
--- berry-free-react-admin-template-3.4.1/src/layout/MainLayout/Sidebar/MenuList/index.js
+++ berry-free-react-admin-template-3.4.1/src/layout/MainLayout/Sidebar/MenuList/index.js
@@ -4,11 +4,21 @@ import { Typography } from '@mui/material';
 // project imports
 import NavGroup from './NavGroup';
 import menuItem from 'menu-items';
-
+import { useAdminStatus } from 'hooks';
+import { getNavItems } from 'menu-items/pages';
 // ==============================|| SIDEBAR MENU LIST ||============================== //
 
 const MenuList = () => {
+  const isAdmin = useAdminStatus();
   const navItems = menuItem.items.map((item) => {
+    if (item.component) { 
+      item = {
+        children: getNavItems(isAdmin),
+        id: 'pages',
+        type: 'group',
+        title: 'Pages' 
+      }
+    }
     switch (item.type) {
       case 'group':
         return <NavGroup key={item.id} item={item} />;
